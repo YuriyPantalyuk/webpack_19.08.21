deae1dfcb7eae797d59e6132760447bf256da215
YuriyPantalyuk <an3611nt@gmail.com>
2021-08-19 23:23:27 +0300

    started, added: html, css, scss, dev-server

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// import '../../node_modules/focus-visible/dist/focus-visible';
+
+// import $ from 'jquery';
+
+// import '../scss/main.scss';
+// import '../index.html';
+
+
+// const a = 5;
+
+// $('body').css('color', 'red');
+
+
+import '../scss/main.scss';
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './js/main.js',
+    output: {
+        filename: `./js/${filename('js')}`,
+        path: path.resolve(__dirname, 'app'),
+        clean: true,
+    },
+    devServer: {
+        historyApiFallback: true,
+        // contentBase: path.resolve(__dirname, 'app'),
+        open: true,
+        hot: true,
+        compress: true,
+        port: 3000,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: `./css/${filename('css')}`
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+        ],
+    },
+}
